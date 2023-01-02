387c2c9275ee55e9bb99056ba39b0983c9ecc1d7
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2023-01-02 20:32:18 +0800

    Add files via upload

---
+++ keliling segitiga.py
@@ -0,0 +1,6 @@
+#mencari keliling segitiga 
+a=int(input("Masukan sisi 1 : "))
+b=int(input("Masukan sisi 2 : "))
+c=int(input("Masukan sisi 3 : "))
+kel = a + b + c
+print(f"Keliling segitiga : {kel}")
